3acb0eead653afc073290257ed869edc2f88585b
Timur Prokopenko <tprokopenko@elvees.com>
2021-09-08 13:32:23 +0300

    mcom03-sbl: Use MIPS baremetal toolchain
    Depends-On: https://gerrit.elvees.com/26891
    Issue: #MCOM03SW-737
    Change-Id: I3af947d432a958d4396ef18421e1aaab8348e20c

--- package/mcom03-sbl/mcom03-sbl.mk
+++ package/mcom03-sbl/mcom03-sbl.mk
@@ -8,15 +8,14 @@ MCOM03_SBL_VERSION = master
 MCOM03_SBL_SITE = ssh://gerrit.elvees.com:29418/mcom03/sbl
 MCOM03_SBL_SITE_METHOD = git
 MCOM03_SBL_LICENSE = GPLv2
-MCOM03_SBL_DEPENDENCIES = arm-trusted-firmware ddrinit uboot
-MCOM03_SBL_INSTALL_TARGET = NO
-MCOM03_SBL_INSTALL_IMAGES = YES
-define MCOM03_SBL_INSTALL_IMAGES_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/sbl-cpu.bin $(BINARIES_DIR)
-endef
+MCOM03_SBL_DEPENDENCIES = host-mips-img-elf-toolchain \
+	arm-trusted-firmware ddrinit uboot
+MCOM03_SBL_INSTALL_TARGET_OPTS = DESTDIR=$(BINARIES_DIR) install/fast
 
-MCOM03_SBL_CONF_OPTS += -DDDRINIT_PATH=$(BINARIES_DIR)/ddrinit-mcom03bub/ddrinit.bin
+MCOM03_SBL_CONF_OPTS += -DDDRINIT_PATH=$(BINARIES_DIR)/ddrinit-$(word 1, $(subst $\",,$(BR2_PACKAGE_DDRINIT_DEFCONFIGS)))/ddrinit.bin
 MCOM03_SBL_CONF_OPTS += -DTFA_PATH=$(BINARIES_DIR)/bl31.bin
 MCOM03_SBL_CONF_OPTS += -DUBOOT_PATH=$(BINARIES_DIR)/u-boot.bin
+MCOM03_SBL_CONF_OPTS += -DCMAKE_TOOLCHAIN_FILE=$(HOST_DIR)/mips-img-elf/2018.09-03/share/cmake/toolchain.cmake
+MCOM03_SBL_CONF_OPTS += -DCMAKE_INSTALL_PREFIX=
 
 $(eval $(cmake-package))
