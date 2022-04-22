cba99aeb74e978c1ef9637e0fdea9b93180ba69a
Roman Chuchelin <rchuchelin@elvees.com>
2022-04-22 11:01:44 +0000

    Remove .git from repos URL
    .git in the URL causes setup_buildroot.py CI script not to detect repos 
    while generating local.mk
    
    Issue: #INFRA-1402
    Change-Id: Ie14b9edad31c293ddc7140498d1d819cd9a68305

--- package/mcom03-linux-utils-python/mcom03-linux-utils-python.mk
+++ package/mcom03-linux-utils-python/mcom03-linux-utils-python.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 MCOM03_LINUX_UTILS_PYTHON_VERSION = master
-MCOM03_LINUX_UTILS_PYTHON_SITE = ssh://gerrit.elvees.com:29418/mcom03/linux-utils.git
+MCOM03_LINUX_UTILS_PYTHON_SITE = ssh://gerrit.elvees.com:29418/mcom03/linux-utils
 MCOM03_LINUX_UTILS_PYTHON_SITE_METHOD = git
 MCOM03_LINUX_UTILS_PYTHON_LICENSE = MIT
 MCOM03_LINUX_UTILS_PYTHON_SETUP_TYPE = setuptools
--- package/mcom03-linux-utils/mcom03-linux-utils.mk
+++ package/mcom03-linux-utils/mcom03-linux-utils.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 MCOM03_LINUX_UTILS_VERSION = master
-MCOM03_LINUX_UTILS_SITE = ssh://gerrit.elvees.com:29418/mcom03/linux-utils.git
+MCOM03_LINUX_UTILS_SITE = ssh://gerrit.elvees.com:29418/mcom03/linux-utils
 MCOM03_LINUX_UTILS_SITE_METHOD = git
 MCOM03_LINUX_UTILS_LICENSE = Proprietary
 MCOM03_LINUX_UTILS_DEPENDENCIES = mpv
