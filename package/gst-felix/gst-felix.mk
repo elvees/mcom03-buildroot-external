e656c98389866f63579d474a7a32fd9b2abc13d6
Andrey Pasyukov <apasyukov@elvees.com>
2022-05-30 18:42:28 +0300

    package/gst-felix: Select mcom03-dmabuf-exporter
    Issue: #MCOM03SW-1093
    Depends-On: https://gerrit.elvees.com/c/mcom03/dmabuf-exporter/+/31159
    Depends-On: https://gerrit.elvees.com/c/mcom03/felix/+/28956
    Change-Id: I3fcfabcaf8221eb3beeb06df6687a976e23b617d

--- package/gst-felix/gst-felix.mk
+++ package/gst-felix/gst-felix.mk
@@ -7,7 +7,7 @@
 GST_FELIX_VERSION = master
 GST_FELIX_SITE_METHOD = git
 GST_FELIX_SITE = ssh://gerrit.elvees.com:29418/lib/gst-felix
-GST_FELIX_DEPENDENCIES = gstreamer1 mcom03-felix sensor-phy
+GST_FELIX_DEPENDENCIES = gstreamer1 mcom03-dmabuf-exporter mcom03-felix sensor-phy
 GST_FELIX_INSTALL_STAGING = YES
 GST_FELIX_SUBDIR = src
 GST_FELIX_CONF_OPTS = -DCI_EXT_DATA_GENERATOR=0
