66a9d0bb56868277e58e2db77bc2da75f430b0e2
Andrey Pasyukov <apasyukov@elvees.com>
2022-06-02 18:01:34 +0300

    package/gst-felix: Add possibility to install from binaries
    New option "Build from source code" was added. If unchecked,
    package will use tarball with binaries for installation.
    
    Issue: #MCOM03SW-1517
    Change-Id: I8406598afdb1c0a83afe1e0981cf048a290fe1b8

--- package/gst-felix/gst-felix.mk
+++ package/gst-felix/gst-felix.mk
@@ -4,14 +4,17 @@
 #
 ################################################################################
 
+GST_FELIX_DEPENDENCIES = gstreamer1 mcom03-dmabuf-exporter mcom03-felix
+
+# Installation from source code
+ifeq ($(BR2_PACKAGE_GST_FELIX_INSTALL_SRC),y)
 GST_FELIX_VERSION = master
 GST_FELIX_SITE_METHOD = git
 GST_FELIX_SITE = ssh://gerrit.elvees.com:29418/lib/gst-felix
-GST_FELIX_DEPENDENCIES = gstreamer1 mcom03-dmabuf-exporter mcom03-felix sensor-phy
 GST_FELIX_INSTALL_STAGING = YES
 GST_FELIX_INSTALL_IMAGES = YES
 GST_FELIX_SUBDIR = src
-GST_FELIX_CONF_OPTS = -DCI_EXT_DATA_GENERATOR=0
+GST_FELIX_CONF_OPTS += -DCI_EXT_DATA_GENERATOR=0
 GST_FELIX_CONF_OPTS += -DFELIX_SOURCE_DIR=$(MCOM03_FELIX_DIR)/DDKSource
 GST_FELIX_CONF_OPTS += -DFELIX_BINARY_DIR=$(MCOM03_FELIX_BUILDDIR)
 GST_FELIX_CONF_OPTS += -DPAGE_SIZE_16K=$(shell grep -wc 'CONFIG_PAGE_SIZE_16KB=y' $(LINUX_DIR)/.config)
@@ -36,3 +39,17 @@ define GST_FELIX_INSTALL_IMAGES_CMDS
 endef
 
 $(eval $(cmake-package))
+
+# Installation from tarball with binaries
+else
+GST_FELIX_VERSION = latest
+GST_FELIX_SITE = http://dist.elvees.com/mcom03/packages/gst-felix
+GST_FELIX_STRIP_COMPONENTS = 0
+
+define GST_FELIX_INSTALL_TARGET_CMDS
+	rsync -aK $(@D)/* $(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
+
+endif
