9d3c0db07fbbe553f391952112dd8decaa2bb51f
Mikhail Konovalov <m.konovalov@trustlab.center>
2022-08-03 12:29:22 +0300

    package/mcom03-sbl-tl: New package
    The package is part of image (SBIMG) to be loaded
    using the BootROM. It is responsible for verifying
    and loading secure images during trusted boot stages.
    
    Issue: #MCOM03SW-1100
    Change-Id: I84632d338a733eb54cd810cdfaefa33b7ebce717

---
+++ package/mcom03-sbl-tl/mcom03-sbl-tl.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# mcom03-sbl-tl
+#
+################################################################################
+
+MCOM03_SBL_TL_VERSION = archiveVideoCam_Ph5
+MCOM03_SBL_TL_SITE = git@git.elvees.com:TrustLab-Engineers-Internal/sbl-tl.git
+MCOM03_SBL_TL_SITE_METHOD = git
+MCOM03_SBL_TL_GIT_SUBMODULES = YES
+MCOM03_SBL_TL_DEPENDENCIES = host-toolchain-mipsel-elvees-elf32
+
+MCOM03_SBL_TL_INSTALL_TARGET = NO
+MCOM03_SBL_TL_INSTALL_IMAGES = YES
+
+MCOM03_SBL_TL_MAKE_OPTS += \
+	CROSS_COMPILE=$(BR2_PACKAGE_MCOM03_SBL_TL_TOOLCHAIN) \
+	$(call qstrip,$(BR2_PACKAGE_MCOM03_SBL_TL_ADDITIONAL_VARIABLES))
+
+define MCOM03_SBL_TL_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) clean; \
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(MCOM03_SBL_TL_MAKE_OPTS);
+endef
+
+define MCOM03_SBL_TL_INSTALL_IMAGES_CMDS
+	cp -f $(@D)/image/sbl-tl.bin $(BINARIES_DIR)
+endef
+
+$(eval $(generic-package))
