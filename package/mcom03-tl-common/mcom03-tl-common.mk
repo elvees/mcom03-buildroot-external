f6764aea180db4bb1be13bde96ca9e7b577f82d7
Mikhail Konovalov <m.konovalov@trustlab.center>
2022-08-04 14:37:05 +0300

    package/mcom03-tl-common: New package
    The package is part of image (SBIMG) to be loaded
    using the sbl-tl. It setups and provides the operation
    of the service subsystem and trusted services.
    
    Issue: #MCOM03SW-1100
    Change-Id: I989eb77ed26c2e9dd6ee0c794cbde6d10ed18c45

---
+++ package/mcom03-tl-common/mcom03-tl-common.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# mcom03-tl-common
+#
+################################################################################
+
+MCOM03_TL_COMMON_VERSION = archiveVideoCam_Ph5
+MCOM03_TL_COMMON_SITE = git@git.elvees.com:TrustLab-Engineers-Internal/tl-common.git
+MCOM03_TL_COMMON_SITE_METHOD = git
+MCOM03_TL_COMMON_GIT_SUBMODULES = YES
+MCOM03_TL_COMMON_DEPENDENCIES = host-toolchain-mipsel-elvees-elf32
+
+MCOM03_TL_COMMON_INSTALL_TARGET = NO
+MCOM03_TL_COMMON_INSTALL_IMAGES = YES
+
+MCOM03_TL_COMMON_MAKE_OPTS += \
+	CROSS_COMPILE=$(BR2_PACKAGE_MCOM03_TL_COMMON_TOOLCHAIN) \
+	$(call qstrip,$(BR2_PACKAGE_MCOM03_TL_COMMON_ADDITIONAL_VARIABLES))
+
+define MCOM03_TL_COMMON_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) clean; \
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(MCOM03_TL_COMMON_MAKE_OPTS);
+endef
+
+define MCOM03_TL_COMMON_INSTALL_IMAGES_CMDS
+	cp -f $(@D)/image/tl-common.bin $(BINARIES_DIR)
+endef
+
+$(eval $(generic-package))
