d515fb920e7f348550eb852e2da0139183841b41
Mikhail Konovalov <mkonovalov@elvees.com>
2023-04-07 11:45:53 +0300

    toolchain-mipsel-elvees-elf32: Update GCC version to r193114_10.2023-04-05
    The toolchain uses the newlib as sources for the standard library.
    The newlib always puts _impure_ptr pointer to small data section:
    
    #if defined(__mips__) && !defined(__rtems__)
    #define __ATTRIBUTE_IMPURE_PTR__ __attribute__((__section__(".sdata")))
    #endif
    
    This makes it required to use "gp" register which leads to relocation
    issue when linking with sources compiled with "-G0" flag:
    
    (.text+0x26c): relocation truncated to fit: R_MIPS_GPREL16 against `_impure_ptr'
    
    This version has been built with "-no-gpt" option to disable using of
    "gp" register at all to solve relocation issue on linking libc.
    
    Issue: #ELVTOOLS-357
    Change-Id: I264f7cd2b374a2a928d0d3931fd94119a8603fad

--- toolchain/toolchain-mipsel-elvees-elf32/toolchain-mipsel-elvees-elf32.mk
+++ toolchain/toolchain-mipsel-elvees-elf32/toolchain-mipsel-elvees-elf32.mk
@@ -4,10 +4,13 @@
 #
 ################################################################################
 
-HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_SOURCE = Elvees-MCom03-MIPS32.GCC-tools-11.2.0.linux64.r184962_6.2022-05-17.tar.gz
+HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_SOURCE = ELVEES-MCom03-Mips32.GCC.linux64.r193114_10.2023-04-05.tar.gz
 HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_SITE = https://diver.elvees.com/pub/tools_linux/mips-tools11.2.0-mcom03
 HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_INSTALL_DIR = $(HOST_DIR)/opt/toolchain-mipsel-elvees-elf32
 HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_CMAKE_FILE = $(HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_INSTALL_DIR)/share/cmake/toolchain.cmake
+
+# site uses self-signed certificates
+HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_DL_OPTS = --no-check-certificate
 
 define HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_INSTALL_CMDS
 	mkdir -p $(dir $(HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_INSTALL_DIR))
