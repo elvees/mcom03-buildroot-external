41e69a8aceea7d58f2d035fc5ecbd507400de675
Mikhail Konovalov <mkonovalov@elvees.com>
2023-04-10 15:39:16 +0300

    package/mcom03-tl-core: New package
    This is fork of tl-common repository.
    
    Issue: #MCOM03SW-2248
    Change-Id: I1f94e8a94b198a10365313f6d4107e827a449ba7

---
+++ package/mcom03-tl-core/mcom03-tl-core.mk
@@ -0,0 +1,57 @@
+################################################################################
+#
+# mcom03-tl-core
+#
+################################################################################
+
+MCOM03_TL_CORE_IMAGE = image/tl-core.bin
+
+MCOM03_TL_CORE_INSTALL_TARGET = NO
+MCOM03_TL_CORE_INSTALL_IMAGES = YES
+
+MCOM03_TL_CORE_DEPENDENCIES = host-toolchain-mipsel-elvees-elf32
+
+# Installation from source code
+ifeq ($(BR2_PACKAGE_MCOM03_TL_CORE_INSTALL_SRC),y)
+MCOM03_TL_CORE_VERSION = $(call qstrip,$(BR2_PACKAGE_MCOM03_TL_CORE_REPO_VERSION))
+MCOM03_TL_CORE_SITE = ssh://gerrit.elvees.com:29418/mcom03/tl-core
+MCOM03_TL_CORE_SITE_METHOD = git
+MCOM03_TL_CORE_GIT_SUBMODULES = YES
+
+MCOM03_TL_CORE_MAKE_OPTS += \
+	CROSS_COMPILE=$(BR2_PACKAGE_MCOM03_TL_CORE_TOOLCHAIN) \
+	$(call qstrip,$(BR2_PACKAGE_MCOM03_TL_CORE_ADDITIONAL_VARIABLES))
+
+define MCOM03_TL_CORE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) clean; \
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(MCOM03_TL_CORE_MAKE_OPTS);
+endef
+
+ifneq ($(MCOM03_TL_CORE_OVERRIDE_SRCDIR),)
+MCOM03_TL_CORE_GIT_DIR = $(MCOM03_TL_CORE_OVERRIDE_SRCDIR)
+else
+MCOM03_TL_CORE_GIT_DIR = $(MCOM03_TL_CORE_DL_DIR)/git
+endif
+
+MCOM03_TL_CORE_TARBALL_VERSION = $(shell git -C $(MCOM03_TL_CORE_GIT_DIR) describe --always || echo "unknown")-$(shell date +%Y%m%d)
+
+define MCOM03_TL_CORE_INSTALL_IMAGES_CMDS
+	cp -f $(@D)/$(MCOM03_TL_CORE_IMAGE) $(BINARIES_DIR)
+	tar -C $(@D) -czf \
+		$(BINARIES_DIR)/mcom03-tl-core-$(MCOM03_TL_CORE_TARBALL_VERSION).tar.gz \
+		$(MCOM03_TL_CORE_IMAGE)
+endef
+
+# Installation from tarball with binaries
+else
+MCOM03_TL_CORE_VERSION = latest
+MCOM03_TL_CORE_SITE = http://dist.elvees.com/mcom03/packages/mcom03-tl-core
+MCOM03_TL_CORE_STRIP_COMPONENTS = 0
+
+define MCOM03_TL_CORE_INSTALL_IMAGES_CMDS
+	cp -f $(@D)/$(MCOM03_TL_CORE_IMAGE) $(BINARIES_DIR)
+endef
+
+endif
+
+$(eval $(generic-package))
