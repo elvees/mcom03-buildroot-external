fba679e73da02a137b7cda9a66ba60b80daf0b9c
Vladimir Terekhov <vterekhov@elvees.com>
2023-04-18 18:23:00 +0300

    package/mcom03-sbl: Build SBL for given ddrinit defconfigs and DTBs
    BR2_PACKAGE_DDRINIT_DEFCONFIGS variable may contain several ddrinit
    defconfigs and we want to build them all for all DTBs provided by
    U-Boot. The path to the config file with possible ddrinit/DTB pairs
    must be passed via BR2_MCOM03_DDRINIT_DTB_MAP variable.
    If certain ddrinit is present in the map file, but hasn't been built,
    sbl build will fail.
    
    Issue: #MCOM03SW-2228
    Change-Id: I58ff1aa54e4b45869379fe9d971712d8cec2c92d

--- package/mcom03-sbl/mcom03-sbl.mk
+++ package/mcom03-sbl/mcom03-sbl.mk
@@ -12,17 +12,10 @@ MCOM03_SBL_DEPENDENCIES = host-toolchain-mipsel-elvees-elf32 \
 	arm-trusted-firmware ddrinit uboot
 MCOM03_SBL_INSTALL_TARGET_OPTS = DESTDIR=$(BINARIES_DIR)/sbl install/fast
 
-MCOM03_SBL_DDRINIT_PLAT = \
-	$(word 1, $(strip $(foreach i, $(call qstrip,$(BR2_PACKAGE_DDRINIT_DEFCONFIGS)), \
-		$(if $(findstring bootrom,$i),,$i))))
-
-MCOM03_SBL_CONF_OPTS += -DDDRINIT_PATH=$(BINARIES_DIR)/ddrinit-$(subst :,-,$(MCOM03_SBL_DDRINIT_PLAT))/ddrinit.bin
-MCOM03_SBL_CONF_OPTS += -DTFA_PATH=$(BINARIES_DIR)/bl31.bin
-ifneq ($(wildcard $(BINARIES_DIR)/bl32.bin),)
-MCOM03_SBL_CONF_OPTS += -DBL32_PATH=$(BINARIES_DIR)/bl32.bin
-endif
-MCOM03_SBL_CONF_OPTS += -DUBOOT_PATH=$(BINARIES_DIR)/u-boot.bin
+MCOM03_SBL_CONF_OPTS += -DIMAGES_PATH=$(BINARIES_DIR)
 MCOM03_SBL_CONF_OPTS += -DCMAKE_TOOLCHAIN_FILE=$(HOST_TOOLCHAIN_MIPSEL_ELVEES_ELF32_CMAKE_FILE)
 MCOM03_SBL_CONF_OPTS += -DCMAKE_INSTALL_PREFIX=
+
+MCOM03_SBL_CONF_OPTS += -DDDRINIT_DTB_MAP="$(shell cat $(BR2_MCOM03_DDRINIT_DTB_MAP))"
 
 $(eval $(cmake-package))
