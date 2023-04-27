f690c0a42484df5b4bc857105def8c35f10a9a3f
Mikhail Konovalov <mkonovalov@elvees.com>
2023-04-27 13:32:08 +0300

    package/bootrom-tools: Change download method
    The package are moved from git.elvees.com to callisto.elvees.com.
    Also it is downloaded as tar archive now.
    
    The tools was imported previously from [1].
    
    [1] - https://diver.elvees.com/pub/tools_linux/Elvees-SDK/MCom03-console-SDK/MCom03-console-SDK.linux64.2022-06-09.tar.gz
    
    Change-Id: I9fa42db5a565c37d9429f22b4b1edc3aeb65ac3e

--- package/bootrom-tools/bootrom-tools.mk
+++ package/bootrom-tools/bootrom-tools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-HOST_BOOTROM_TOOLS_VERSION = master
-HOST_BOOTROM_TOOLS_SITE = git@git.elvees.com:TrustLab-Engineers-Internal/bootrom_tools.git
-HOST_BOOTROM_TOOLS_SITE_METHOD = git
+HOST_BOOTROM_TOOLS_VERSION = 2022-06-09
+HOST_BOOTROM_TOOLS_SOURCE = bootrom-tools.$(HOST_BOOTROM_TOOLS_VERSION).tar.gz
+HOST_BOOTROM_TOOLS_SITE = https://callisto.elvees.com/share/mcom03/boottools
 
 HOST_BOOTROM_TOOLS_INSTALL_DIR = $(HOST_DIR)/opt/bootrom-tools
 
