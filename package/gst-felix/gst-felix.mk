9edd3938e9e415960de50a27af45006ab9a30756
Andrey Khamukhin <ahamuhin@elvees.com>
2026-05-19 09:55:03 +0300

    package/gst-felix: Remove dependency from lenscontrol
    All metadata generated by felixsrc is now placed in gst-felix package.
    
    Issue: #IPCAMSW-4315
    Change-Id: I61d4e0b66f92a2daa2652f786d2a882b7ea96e40
    (cherry picked from commit a07a80b5d81ae08128626fdb59d06d07757dabd5)

--- package/gst-felix/gst-felix.mk
+++ package/gst-felix/gst-felix.mk
@@ -29,14 +29,10 @@ GST_FELIX_CONF_OPTS += -DFELIX_BINARY_DIR=$(MCOM03_FELIX_BUILDDIR)
 GST_FELIX_CONF_OPTS += -DPAGE_SIZE_16K=$(shell grep -wc 'CONFIG_PAGE_SIZE_16KB=y' $(LINUX_DIR)/.config)
 
 GST_FELIX_TARGET_FILES = \
+	lib/cmake/gstfelixmeta/ \
 	lib/gstreamer-1.0/libfelixsrc.so \
 	lib/libgstfelixmeta.so \
 	usr/include/gstreamer-1.0/gst/gstfelixmeta.h
-
-ifeq ($(BR2_PACKAGE_LENSCONTROL_GST_LENSCONTROL),y)
-GST_FELIX_DEPENDENCIES += lenscontrol
-GST_FELIX_CONF_OPTS += -DGST_LENSCONTROL_META=ON
-endif
 
 ifeq ($(BR2_LINUX_KERNEL),y)
 GST_FELIX_CONF_OPTS += -DLINUX_KERNEL_BUILD_DIR=$(LINUX_DIR)
